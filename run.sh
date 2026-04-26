07faf87e963f1fac6efd12e9f6be07621b7cf714
Andrew Allan Brain <braiaa02@pfw.edu>
2026-04-26 15:26:45 -0400

    Refactor run.sh to use command variable for execution
    Removed installation commands and replaced Java execution with a command variable.

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
-# sudo apt install gcc-aarch64-linux-gnu
-# sudo apt install qemu-user
-# sudo apt install libc6-arm64-cross
-
 aarch64-linux-gnu-gcc -static -o timer timer.s
-/home/ubuntu/.jdks/openjdk-26.0.1/bin/java -javaagent:/opt/intellij/lib/idea_rt.jar=41693 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/ubuntu/.m2/repository/org/openjfx/javafx-controls/21.0.6/javafx-controls-21.0.6.jar:/home/ubuntu/.m2/repository/org/openjfx/javafx-graphics/21.0.6/javafx-graphics-21.0.6.jar:/home/ubuntu/.m2/repository/org/openjfx/javafx-base/21.0.6/javafx-base-21.0.6.jar:/home/ubuntu/.m2/repository/org/openjfx/javafx-fxml/21.0.6/javafx-fxml-21.0.6.jar -p /home/ubuntu/.m2/repository/org/openjfx/javafx-base/21.0.6/javafx-base-21.0.6-linux.jar:/home/ubuntu/Documents/pattern/gridPane/target/classes:/home/ubuntu/.m2/repository/org/openjfx/javafx-graphics/21.0.6/javafx-graphics-21.0.6-linux.jar:/home/ubuntu/.m2/repository/org/openjfx/javafx-fxml/21.0.6/javafx-fxml-21.0.6-linux.jar:/home/ubuntu/.m2/repository/org/openjfx/javafx-controls/21.0.6/javafx-controls-21.0.6-linux.jar -m gridgui.gridpane/gridgui.gridpane.GridApplication 2> /dev/null &
+command="<COPY_HERE>"
+$command 2> /dev/null &
 processID=$! 
 trap "kill $processID" EXIT
 qemu-aarch64 ./timer
